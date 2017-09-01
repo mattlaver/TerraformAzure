43b88e59d4e5ae8dcf0422d55553fd936e304909
mattlaver <matt.laver@uk.tesco.com>
2017-09-01 23:10:08 +0100

    Resource Group Creation

---
+++ Examples/Example1/vars.tf
@@ -0,0 +1,12 @@
+variable "subscription_id" {}
+variable "client_id" {}
+variable "client_secret" {}
+variable "tenant_id" {}
+
+variable "resource_group_name" {
+  default = "myapps-dev-rg"
+}
+
+variable "location" {
+  default = "UK South"
+}
